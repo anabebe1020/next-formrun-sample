603d4b316364f55aff7e5b5c48215dec0d569bae
Ryousuke Abe <rabe1020.biz@gmail.com>
2025-05-09 13:05:51 +0900

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
+// const FORMRUN_API_URL = "https://form.run/api/v1/forms/abe-r-5kuuN4VL0zPjjgCV04K2";
+const FORMRUN_API_URL = "https://form.run/embed/@abe-r-5kuuN4VL0zPjjgCV04K2?embed=direct";
+
 export default function Home() {
+  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   try {
+  //     event.preventDefault();
+  //     const form = document.getElementById("custom-form") as HTMLFormElement;
+  //     const formData = new FormData(form);
+  //     const data: { [key: string]: string } = {};
+  //     formData.forEach((value, key) => {
+  //       if (key === "_field_3" || key === "_field_4") {
+  //         data[key] = value as string;
+  //       }
+  //     });
+  //     console.log(data);
+  //     await fetch(FORMRUN_API_URL, {
+  //       method: "POST",
+  //       body: JSON.stringify(data),
+  //     });
+  //   } catch (error) {
+  //     console.error("Error submitting form:", error);
+  //   }
+  // };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -13,82 +39,25 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <form
+          id="custom-form"
+          action={FORMRUN_API_URL}
+          method="POST"
+          // onSubmit={handleSubmit}
+        >
+          <input type="text" name="_field_3" placeholder="テキストA" required />
+          <input type="text" name="_field_4" placeholder="テキストB" required />
+
+          {/* <input type="hidden" name="_redirect" value="https://form.run/@abe-r-5kuuN4VL0zPjjgCV04K2/thanks" /> */}
+          <input type="hidden" name="_redirect" value="./sent" />
+
+          <button type="submit">送信</button>
+        </form>
+
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
---
+++ src/app/sent/page.tsx
@@ -0,0 +1,8 @@
+export default function SentPage() {
+  return (
+    <div>
+      <h1>送信完了</h1>
+      <p>フォームの送信が完了しました。</p>
+    </div>
+  );
+}
